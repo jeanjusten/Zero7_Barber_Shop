268446cb89b0ad2cc50b45345d594452d345f0e1
Jean Justen <jean.cjm@hotmail.com>
2024-09-03 15:56:10 -0300

    Fixed whatsapp links, curved sections

--- zero7.js
+++ zero7.js
@@ -15,6 +15,23 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
+// Closes the dropdown menu after clicking.
+document.addEventListener('DOMContentLoaded', function() {
+    let navLinks = document.querySelectorAll(".nav-link");
+    let navbarCollapse = document.querySelector(".navbar-collapse");
+
+    navLinks.forEach(function(link) {
+        link.addEventListener("click", function() {
+            // Delay for closing navbar dropdown menu
+            setTimeout(function() {
+                if (navbarCollapse.classList.contains("show")) {
+                    new bootstrap.Collapse(navbarCollapse).toggle();
+                }
+            }, 1000); // 1 second
+        });
+    });
+});
+
 // #endregion ---------------------------------------------------------------------------------------//
 
 
